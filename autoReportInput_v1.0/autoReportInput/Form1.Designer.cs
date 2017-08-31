2562ad245d819a02c0280ad4dcfc41056b12b416
Administrator <admin@example.com>
2017-08-31 11:34:34 +0900

    third commit

---
+++ autoReportInput_v1.0/autoReportInput/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace autoReportInput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "*秒後、入力開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 19);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 19);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "入力開始までの秒数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "繰り返し入力回数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "入力文字列";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(12, 248);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 19);
+            this.textBox4.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 226);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "減算値";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(114, 181);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "運転時間";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "：";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(129, 201);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 19);
+            this.textBox5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 289);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "反復入力";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ autoReportInput_v1.0/autoReportInput/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace autoReportInput
+{
+    public partial class Form1 : Form
+    {
+        bool time_flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //表示文字更新
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Text = textBox1.Text + "秒後、入力開始";
+        }
+
+        //入力開始ボタンクリック
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //初期化
+            time_flag = false;
+
+            //未入力検出
+            if ((textBox1.Text == "" | textBox2.Text == "" | textBox3.Text == "") || 
+                checkBox1.Checked == true && textBox4.Text == "" ||
+                checkBox2.Checked == true && textBox5.Text == "")
+            {
+                MessageBox.Show("未入力項目があります");
+                return;
+            }
+
+            //時間検出
+            //if (textBox3.Text.IndexOf(":") != -1 || textBox3.Text.IndexOf("：") != -1)
+            //{
+            //    //5文字かつ真ん中にコロンでなければ不正
+            //    if (textBox3.Text.Length != 5 || (textBox3.Text.Substring(2, 1) != ":" && textBox3.Text.Substring(2, 1) != "："))
+            //    {
+            //        MessageBox.Show("値が不正です");
+            //        return;
+            //    }
+
+            //    int hour_s,time_s;
+            //    hour_s = int.Parse(textBox3.Text.Substring(0,2));
+            //    time_s = int.Parse(textBox3.Text.Substring(3,2));
+
+            //    //コロンの左右が0<x<60でなければ不正
+            //    if (hour_s < 0 || hour_s > 60 || time_s < 0 || time_s > 60)
+            //    {
+            //        MessageBox.Show("値が不正です");
+            //        return;
+            //    }
+
+            //    time_flag = true;
+            //}
+
+            double intnum=0;
+            int unt1=0,unt2=0;
+            string st="";
+
+            //指定時間待ち
+            System.Threading.Thread.Sleep(int.Parse(textBox1.Text) * 1000);
+
+            //繰り返し入力処理開始
+            if (checkBox1.Checked == true)
+            {
+                //減算あり時処理
+                if (checkBox2.Checked == true)
+                {
+                    //時間減算処理
+                    for (int num = 0; num < int.Parse(textBox2.Text); num++)
+                    {
+                        unt1=int.Parse(textBox3.Text);
+                        unt2=int.Parse(textBox5.Text) - (num*int.Parse(textBox4.Text));
+
+                        if (unt2 < 0)
+                        {
+                            unt1 = unt1 - 1;
+                            unt2 = 60 + unt2;
+                        }
+
+                        st = unt1.ToString() + ":" + unt2.ToString();
+                        SendKeys.Send(st);
+                        SendKeys.Send("{ENTER}");
+                    }
+                }
+                else
+                {
+                    //時間以外の減算処理
+                    for (int num = 0; num < int.Parse(textBox2.Text); num++)
+                    {
+                        intnum = double.Parse(textBox3.Text) - (num * double.Parse(textBox4.Text));
+                        st = intnum.ToString();
+                        SendKeys.Send(st);
+                        SendKeys.Send("{ENTER}");
+                    }
+                }
+            }
+            else
+            {
+                //減算なし時処理
+                for (int num = 0; num < int.Parse(textBox2.Text); num++)
+                {
+                    SendKeys.Send(textBox3.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                //押されたキーが 0～9でない場合は、イベントをキャンセルする
+                e.Handled = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //減算チェック
+            textBox4.Enabled = !textBox4.Enabled;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //運転時間チェック
+            textBox5.Enabled = !textBox5.Enabled;
+        }
+    }
+}
